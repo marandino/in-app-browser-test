87e9771c4adc19765a22066db5d9fc99458bbb75
Chiyen Venegas <chi@marandino.dev>
2022-07-05 17:26:42 -0500

    add a way to check if the user is using ig

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let userAgentEl = document.getElementById("userAgentParagraph");
 let userAgentDataEl = document.getElementById("userAgentDataParagraph");
 const { userAgent, userAgentData } = navigator;
-userAgentEl.innerText = userAgent;
+userAgentEl.innerText = 'Are you on Instagram? :' + userAgent.includes("Instagram");
 userAgentDataEl.innerText = JSON.stringify(userAgentData);
